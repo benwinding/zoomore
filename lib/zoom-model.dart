079d5e70cdf3ecc2590a2504217fbade074f7477
Ben Winding <hello@benwinding.com>
2020-08-26 21:35:20 +0930

    Better model presevervation

--- lib/zoom-model.dart
+++ lib/zoom-model.dart
@@ -8,12 +8,12 @@ class ZoomModel with ChangeNotifier, DiagnosticableTreeMixin {
   get matrix => this._matrix;
 
   reset() {
-    this._matrix = Matrix4.identity();
+    this._matrix.setIdentity();
     notifyListeners();
   }
 
   void setMatrix(Matrix4 matrixInput) {
-    this._matrix = matrixInput;
+    this._matrix.setFrom(matrixInput);
     notifyListeners();
   }
 }
