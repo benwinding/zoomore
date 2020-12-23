9563097620a8e145fa208d38c928bb843b80c63c
Ben Winding <hello@benwinding.com>
2020-12-23 12:21:06 +1030

    Add landing page

---
+++ landing-page/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+export default function Page() {
+  return (
+    <div>
+      <Head>
+        <title>Zoomore</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <script data-ad-client="ca-pub-2064705087920661" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+      </Head>
+      <h1>Zoomore</h1>
+    </div>  
+  )
+}
