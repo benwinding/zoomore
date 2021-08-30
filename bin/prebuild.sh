f99c77e64576ec326933af856c1bdd274eca5d19
Ben Winding <hello@benwinding.com>
2021-08-31 00:32:09 +1000

    Fix prebuild

--- bin/prebuild.sh
+++ bin/prebuild.sh
@@ -4,5 +4,5 @@ flutter clean
 flutter pub get
 flutter pub pub run flutter_native_splash:create
 flutter pub run flutter_launcher_icons:main
-./bin/version-bump.sh
 bin/ensure-newline.sh ./ios/Runner.xcodeproj/project.pbxproj
+./bin/version-bump.sh
