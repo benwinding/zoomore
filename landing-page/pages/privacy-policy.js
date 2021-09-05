d63b360cb69e1abc0f8f0be8a7d349675336901b
Ben Winding <hello@benwinding.com>
2021-09-05 20:59:20 +1000

    Update privacy

--- landing-page/pages/privacy-policy.js
+++ landing-page/pages/privacy-policy.js
@@ -37,10 +37,7 @@ export default function Page() {
           <strong>Information Collection and Use</strong>
         </p>
         <p>
-          For a better experience, while using our Service, I may require you to
-          provide us with certain personally identifiable information. The
-          information that I request is retained on your device and is not
-          collected by me in any way
+          This app does not collect user generated information in any way.
         </p>
         <p>
           The app does use third party services that may collect information
@@ -59,42 +56,6 @@ export default function Page() {
           Service, and other statistics.
         </p>
         <p>
-          <strong>Cookies</strong>
-        </p>
-        <p>
-          Cookies are files with a small amount of data that are commonly used
-          as anonymous unique identifiers. These are sent to your browser from
-          the websites that you visit and are stored on your device's internal
-          memory.
-        </p>
-        <p>
-          This Service does not use these “cookies” explicitly. However, the app
-          may use third party code and libraries that use “cookies” to collect
-          information and improve their services. You have the option to either
-          accept or refuse these cookies and know when a cookie is being sent to
-          your device. If you choose to refuse our cookies, you may not be able
-          to use some portions of this Service.
-        </p>
-        <p>
-          <strong>Service Providers</strong>
-        </p>
-        <p>
-          I may employ third-party companies and individuals due to the
-          following reasons:
-        </p>
-        <ul>
-          <li>To facilitate our Service;</li>
-          <li>To provide the Service on our behalf;</li>
-          <li>To perform Service-related services; or</li>
-          <li>To assist us in analyzing how our Service is used.</li>
-        </ul>
-        <p>
-          I want to inform users of this Service that these third parties have
-          access to your Personal Information. The reason is to perform the
-          tasks assigned to them on our behalf. However, they are obligated not
-          to disclose or use the information for any other purpose.
-        </p>
-        <p>
           <strong>Security</strong>
         </p>
         <p>
@@ -103,29 +64,6 @@ export default function Page() {
           But remember that no method of transmission over the internet, or
           method of electronic storage is 100% secure and reliable, and I cannot
           guarantee its absolute security.
-        </p>
-        <p>
-          <strong>Links to Other Sites</strong>
-        </p>
-        <p>
-          This Service may contain links to other sites. If you click on a
-          third-party link, you will be directed to that site. Note that these
-          external sites are not operated by me. Therefore, I strongly advise
-          you to review the Privacy Policy of these websites. I have no control
-          over and assume no responsibility for the content, privacy policies,
-          or practices of any third-party sites or services.
-        </p>
-        <p>
-          <strong>Children’s Privacy</strong>
-        </p>
-        <p>
-          These Services do not address anyone under the age of 13. I do not
-          knowingly collect personally identifiable information from children
-          under 13. In the case I discover that a child under 13 has provided me
-          with personal information, I immediately delete this from our servers.
-          If you are a parent or guardian and you are aware that your child has
-          provided us with personal information, please contact me so that I
-          will be able to do necessary actions.
         </p>
         <p>
           <strong>Changes to This Privacy Policy</strong>
