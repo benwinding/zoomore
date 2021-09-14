51ddb97a8e453c7f0c2b55cba858832a32282cdb
Ben Winding <hello@benwinding.com>
2021-09-14 14:55:32 +1000

    Fix version script

--- bin/utils/version-bump.sh
+++ bin/utils/version-bump.sh
@@ -12,10 +12,11 @@ fi
 UTILS_DIR=$(dirname $0)
 PUBSPEC_PATH="$UTILS_DIR/../../pubspec.yaml"
 echo "Using pubspec file: $PUBSPEC_PATH"
-# Find and increment the version number.
-perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' $PUBSPEC_PATH
+# Find and increment the patch version number: 0.0.x
+perl -i -pe 's/^(version:\s+\d+\.\d+\.)(\d+)$/$1.($2+1)/e' $PUBSPEC_PATH
 
 # Commit and tag this change.
 version=`grep 'version: ' $PUBSPEC_PATH | sed 's/version: //'`
+echo "Tagging version: $version"
 git commit -m "Bump version to $version" $PUBSPEC_PATH
 git tag $version
