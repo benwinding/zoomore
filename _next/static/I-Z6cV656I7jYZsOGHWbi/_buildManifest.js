87ba2a4ed27afc18734461d9a73b3aefca29c53f
benwinding <benwinding@users.noreply.github.com>
2026-02-28 07:20:07 +0000

    deploy: a8b349540c4b3d246fce014143e37715f1cddab8

--- _next/static/I-Z6cV656I7jYZsOGHWbi/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-525f9ac459ee04845ed3.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-bb1d1693548b1ecdf27b.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
